ac124e101d6bd6bcc3505fd1f809a061c2144d16
M. S <aliahmedsiddiqui48@gmail.com>
2023-08-16 12:37:12 +0500

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,575 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+
+ <div className='post1'>
+<div className='nav'>
+
+
+  <ul className='list'>
+    <li className='logo'> DORSIN </li>
+    <li><a href='#aaa'>HOME</a></li>
+    <li><a href='#bbb'>SERVICES</a></li>
+    <li><a href='#ccc'>FEATURE</a></li>
+    <li><a href='#ddd'>PRICING</a></li>
+    <li><a href='#eee'>TEAM</a></li>
+    <li><a href='#fff'>BLOG</a></li>
+    <li><a href='#ggg'>CONTACT</a></li>
+<li ><button className='btn'>TRY IT FREE</button></li> 
+
+  </ul> 
+
+
+</div> 
+
+
+<div id='aaa' className='post'>
+
+<p className='big'>We help startups launch their products</p>
+<p className='small'>Etiam sed.Interdum consequat proin vestibulum class at.</p>
+
+</div>
+</div>
+
+
+
+<div className='post2'>
+  <div className='cet'>
+<h1 id='bbb' >OUR SERVICES </h1><br /> ____ < br/>< br/>< br/>
+<p className='head'> We craft digital, graphic and dimensional thinking, to create category leading brand experiences that have meaning and add a value for our clients.</p>
+</div>
+
+
+
+
+<div className='mainly'>
+
+<div className='card'>
+<h1 className='head1'>Digital Design</h1>
+<p className='pa'>Some quick example text to build on the card title and make up the bulk of the card's content. Moltin gives you the platform.</p>
+</div>
+
+
+<div className='card'>
+<h1 className='head1'>Unlimited Colors</h1>
+<p className='pa'>Credibly brand standards compliant users without extensible services. Anibh euismod tincidunt ut laoreet.</p>
+</div>
+
+<div className='card'>
+<h1 className='head1'>Strategy Solutions</h1>
+<p className='pa'>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean necessary regelialia.</p>
+</div>
+
+
+
+</div>
+<div className='mainly'>
+
+
+<div className='card'>
+<h1 className='head1'>Awesome Support</h1>
+<p className='pa'>
+It is a paradisematic country, in which roasted parts of sentences fly into your mouth leave for the far World.</p>
+</div>
+
+<div className='card'>
+<h1 className='head1'>Truly Multipurpose</h1>
+<p className='pa'>
+Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+</div>
+
+<div className='card'>
+<h1 className='head1'>Easy to customize</h1>
+<p className='pa'>
+Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia.</p>
+</div>
+</div>
+
+
+
+
+</div>
+
+
+
+
+
+<div id='ccc'   className='post3'>
+
+<div className='left'>
+  <h1 className='hello'>
+  A digital web design studio creating modern & engaging online experiences
+  </h1><br /><br />
+  <p className='hello'>
+  Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.
+  </p><br /><br />
+  <ul className='hello'>
+<li>We put a lot of effort in design.</li> <br />
+<li>The most important ingredient of successful website</li><br />
+<li>Sed ut perspiciatis unde omnis iste natus error sit.</li><br />
+<li>Submit Your Organization.</li><br />
+
+  </ul>
+  <br />
+<button className='btn1'>LEARN MORE</button>
+</div>
+<div className='right' >
+
+<img width="100%" src='http://dorsin.react.themesbrand.com/assets/images/online-world.svg'></img>
+  
+</div>
+
+
+
+
+
+
+</div>
+
+
+
+
+
+<div className='post4'>
+  
+<div className='text'>
+  
+  <h1>Build your dream website today</h1><br />
+<p>But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her</p>
+<br /><br />
+<button className='butn'>VEIW PLAN AND PRICING</button>
+  
+  
+  
+  
+   </div>
+
+</div>
+
+
+
+
+
+
+
+
+<div className='post5'>
+
+<h1 id='ddd' >OUR PRICING</h1><br /> ____ < br/>< br/>< br/>
+<p>Call to action pricing table is really crucial to your for your business website. Make your bids stand-out with amazing options.</p>
+
+<div className='cardm'> 
+
+<div className='carm'>
+<h4 >ECONOMY</h4><br />
+<h2>$9.90</h2><br />
+<p>BILLING PER MONTH</p><br /><br />
+<hr /><br /><br />
+<p>Bandwidth: 1GB</p><br />
+<p>Onlinespace: 50MB</p><br />
+<p>Support: No</p><br />
+<p>1 Domain</p><br />
+<p>No Hidden Fees</p><br />
+
+<button className='btn1'>JOIN NOM</button>
+
+
+
+</div>
+
+<div className='carm'>
+<h4 >ECONOMY</h4><br />
+<h2>$9.90</h2><br />
+<p>BILLING PER MONTH</p><br /><br />
+<hr /><br /><br />
+<p>Bandwidth: 1GB</p><br />
+<p>Onlinespace: 50MB</p><br />
+<p>Support: No</p><br />
+<p>1 Domain</p><br />
+<p>No Hidden Fees</p><br />
+<button className='btn1'>JOIN NOM</button>
+
+
+
+
+
+</div>
+<div className='carm'>
+<h4 >ECONOMY</h4><br />
+<h2>$9.90</h2><br />
+<p>BILLING PER MONTH</p><br /><br />
+<hr /><br /><br />
+<p>Bandwidth: 1GB</p><br />
+<p>Onlinespace: 50MB</p><br />
+<p>Support: No</p><br />
+<p>1 Domain</p><br />
+<p>No Hidden Fees</p><br />
+
+<button className='btn1'>JOIN NOM</button>
+
+
+
+
+</div>
+
+</div>
+
+
+
+
+<h1 id='eee'>BEHIND THE PEOPLE</h1><br /> ____ < br/>< br/>< br/>
+<p>It is a long established fact that create category leading brand experiences a reader will be distracted by the readable content of a page when looking at its layout.</p>
+
+< br/>< br/>< br/>
+
+<div className='maine'>
+
+
+<div className='cardw'>
+  
+  <img width="100%" src='http://dorsin.react.themesbrand.com/assets/images/team/img-1.jpg' /><br />
+  <h3>Frank Johnson</h3><br />
+  <h6>CEO</h6>
+   </div>
+   <div className='cardw'>
+  
+  <img width="100%" src='http://dorsin.react.themesbrand.com/assets/images/team/img-2.jpg' /><br />
+  <h3>Elaine Stclair</h3><br />
+  <h6>DESIGNER</h6>
+   </div><div className='cardw'>
+  
+  <img width="100%" src='http://dorsin.react.themesbrand.com/assets/images/team/img-3.jpg' />
+  <h3>Wanda Arthur</h3><br />
+  <h6>DEVELOPER</h6>
+   </div>
+   <div className='cardw'>
+  
+  <img width="100%" src='http://dorsin.react.themesbrand.com/assets/images/team/img-4.jpg' /><br />
+  <h3>Joshua Stemple</h3><br />
+  <h6>MANAGER</h6>
+   </div>
+
+</div>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+<div className='post6'>
+<h1>WORK PROCESS</h1><br /> ____ < br/>< br/>< br/>
+<p>In an ideal world this website wouldn’t exist, a client would acknowledge the importance of having web copy before the Proin vitae ipsum vel ex finibus semper design starts</p>
+
+<div className='maine'>
+
+
+<div className='cardw'>
+  
+  
+  
+  <h3>Tell us what you need</h3><br />
+  <h6>The Big Oxmox advised her not to do so.</h6>
+   </div>
+   
+   
+   <div className='cardw'>
+  
+  
+  <h3>Get free quotes</h3><br />
+  <h6>Little Blind Text didn’t listen.</h6>
+   </div>
+   
+   <div className='cardw'>
+ 
+  <h3>
+Deliver high quality product</h3><br />
+  <h6>When she reached the first hills.</h6>
+   </div>
+
+</div>
+
+
+<button className='btn1'>GET STARTED</button>
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+<div className='post7'>
+
+
+<h1>WHAT THEY'VE SAID</h1><br /> ____ < br/>< br/>< br/>
+<p>The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious pulvinar metus molestie sed Semikoli.</p>
+
+
+<div className='team'>
+
+
+<div className='teamc'> 
+
+<img className='user' src='http://dorsin.react.themesbrand.com/assets/images/testimonials/user-2.jpg' />
+
+<p className='para'>
+I feel confident imposing change on myself. It's a lot more fun progressing than looking back. That's why scelerisque pretium dolor, sit amet vehicula erat pelleque need throw curve balls.”
+</p>
+RUBEN REED - Charleston
+ </div>
+
+
+ <div  className='teamc'> 
+
+<img className='user' src='http://dorsin.react.themesbrand.com/assets/images/testimonials/user-1.jpg' />
+
+<p className='para'>
+I feel confident imposing change on myself. It's a lot more fun progressing than looking back. That's why scelerisque pretium dolor, sit amet vehicula erat pelleque need throw curve balls.”
+</p>
+RUBEN REED - Charleston
+ </div>
+ <div className='teamc'> 
+
+<img className='user' src='http://dorsin.react.themesbrand.com/assets/images/testimonials/user-3.jpg' />
+
+<p className='para'>
+I feel confident imposing change on myself. It's a lot more fun progressing than looking back. That's why scelerisque pretium dolor, sit amet vehicula erat pelleque need throw curve balls.”
+</p>
+RUBEN REED - Charleston
+ </div>
+
+
+
+
+
+</div>
+
+
+
+</div>
+
+
+<div className='post8'>
+  
+<div className='text'>
+  
+  <h1>Build your dream website today</h1><br />
+<p>But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her</p>
+<br /><br />
+<button className='butn'>VEIW PLAN AND PRICING</button>
+  
+  
+  
+  
+   </div>
+
+</div>
+
+
+
+
+<div className='post9'>
+
+<div className='title'>
+
+<h1 id='fff' >BLOG</h1><br /> ____ < br/>< br/>< br/>
+<p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean class at a euismod mus ipsum vel ex finibus semper luctus quam.</p>
+</div>
+<div className='cardm'> 
+
+<div className='carme'>
+  <img width="100%" src='http://dorsin.react.themesbrand.com/assets/images/blog/img-1.jpg'/>
+<p>UI & UX Design</p>
+<h2>Doing a cross country road trip</h2>
+<p>She packed her seven versalia, put her initial into the belt and made herself on the way..</p>
+
+
+<button className='btn1'>Read More </button>
+
+
+
+</div>
+
+<div className='carme'>
+<img width="100%" src='http://dorsin.react.themesbrand.com/assets/images/blog/img-2.jpg'/>
+
+<p>UI & UX Design</p>
+<h2>Doing a cross country road trip</h2>
+<p>She packed her seven versalia, put her initial into the belt and made herself on the way..</p>
+
+
+<button className='btn1'>Read More </button>
+
+
+
+</div>
+<div className='carme'>
+<img width="100%" src='http://dorsin.react.themesbrand.com/assets/images/blog/img-3.jpg'/>
+
+<p>UI & UX Design</p>
+<h2>Doing a cross country road trip</h2>
+<p>She packed her seven versalia, put her initial into the belt and made herself on the way..</p>
+
+
+<button className='btn1'>Read More </button>
+
+
+
+</div>
+</div>
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+<div className='post10'>
+
+
+<h1 id='ggg'>GET IN TOUCH</h1><br /> ____ < br/>< br/>< br/>
+<p>We thrive when coming up with innovative ideas but also understand that a smart concept should be supported with faucibus sapien odio measurable results.</p>
+
+
+<div className='form'>
+
+<div className='left1'> 
+
+
+Office Address 1:<br /><br />
+4461 Cedar Street Moro, AR 72368
+<br /><br />
+Office Address 2:<br /><br />
+2467 Swick Hill Street<br />
+New Orleans, LA 70171
+<br /><br />
+Working Hours:<br /><br />
+9:00AM To 6:00PM
+<br /><br />
+
+ </div>
+<div className='right1'>
+
+  <input className='input' type='text' placeholder='ENTER YOUR NAME' />
+  <input className='input' type='email' placeholder='ENTER YOUR EMAIL' />
+  <input className='input1' type='text' placeholder='ENTER SUBJECT' />
+  <textarea rows="10" placeholder='ENTER YOUR MESSAGE' className='area' > 
+
+  </textarea><br />
+<button className='btn1'>SEND MESSAGE</button>
+
+</div>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+<div className='post11'>
+  
+  <div>
+<h2>  DORSIN</h2><br /><br />
+Home<br />
+About us<br />
+Careers<br />
+Contact us<br />
+  </div>
+  
+  
+
+  <div>
+<h2>  Information</h2><br /><br />
+Terms & Condition<br />
+About us<br />
+Jobs<br />
+Bookmarks<br />
+</div>
+  
+  <div>
+<h2>  Support</h2><br /><br />
+FAQ<br />
+Contact<br />
+Disscusion<br />
+  </div>
+  
+  <div>
+<h2> subscribe</h2><br /><br />
+In an ideal world<br /> this text wouldn’t exist, a <br />client would acknowledge the <br />importance of having web<br /> copy before the design starts.<br />
+  <input className='last' placeholder='ENTER EMAIL'></input>
+  </div>
+  
+  
+
+
+
+  
+  
+   </div>
+
+
+
+
+<div className='pic'>
+
+<div className='left'>
+
+2019 - 2023 © Dorsin - Themesbrand
+
+payment
+
+</div>
+<div className='right'>
+  <img width="100%" className='im' src='http://dorsin.react.themesbrand.com/assets/images/payment.png'/>
+</div>
+
+</div>
+
+
+
+
+
+
+
+
     </div>
   );
 }
